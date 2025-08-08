3aabcbded62d46b93ed60c536f3788d5664a86ae
yunoqii <emptpock@gmail.com>
2025-08-08 11:58:15 +0200

    Ready

--- factory/ERC20AirDropper.sol
+++ factory/ERC20AirDropper.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IUtilityContract.sol";
 
-contract ERC20Airdropper {
+contract ERC20Airdropper is IUtilityContract {
     // ["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db","0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB"]
     // [20000000000000000000000, 30000000000000000000000, 10000000000000000000000]
 
