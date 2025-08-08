fe96913cddedcabfb800a7b38d083997d3ea3aa8
yunoqii <emptpock@gmail.com>
2025-08-08 12:22:03 +0200

    ERC20Airdroper added

--- contracts/ERC20Mock.sol
+++ contracts/ERC20Mock.sol
@@ -5,10 +5,10 @@ pragma solidity ^0.8.29;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MyToken is ERC20, ERC20Permit {
+contract ERC20Mock is ERC20, ERC20Permit {
     constructor(address recipient)
-        ERC20("MyToken", "MTK")
-        ERC20Permit("MyToken")
+        ERC20("ERC20Mock", "MTK")
+        ERC20Permit("ERC20Mock")
     {
         _mint(recipient, 10000 * 10 ** decimals());
     }
