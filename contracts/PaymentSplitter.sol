4fc60d3dcf61305ed0f2cd417b871082400f1a19
Zhenia <117851388+yunoqii@users.noreply.github.com>
2025-09-06 10:40:58 +0200

    deleting trash

--- contracts/PaymentSplitter.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.29;
-
-import "@openzeppelin/contracts/finance/VestingWallet.sol";
-
-contract RevenueSplitter is VestingWallet {
-
-    constructor(address[] memory payees_, uint256[] memory shares_) PaymentSplitter(payees_, shares_) payable {}
-}
